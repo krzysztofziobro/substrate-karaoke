64d8ed08c5ec5e3154dedf65e9ad5dbaca19617a
Krzysztof Ziobro <krzysztof.ziobro@cardinals.cc>
2022-05-04 14:30:26 +0200

    Karaoke: refactor

--- pallets/karaoke/src/lib.rs
+++ pallets/karaoke/src/lib.rs
@@ -5,22 +5,71 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// <https://docs.substrate.io/v3/runtime/frame>
 
+extern crate alloc;
 pub use pallet::*;
+
+use codec::{Decode, Encode};
+use sp_inherents::{InherentData, InherentIdentifier,   IsFatalError};
+use alloc::vec::*;
+
+pub const INHERENT_IDENTIFIER : InherentIdentifier = *b"karaoke0";
+pub type InherentType = Vec<u8>;
+
+pub const SONG_LEN : u32 = 4;
+
+#[derive(Encode, sp_runtime::RuntimeDebug)]
+#[cfg_attr(feature = "std", derive(Decode, thiserror::Error))]
+#[cfg_attr(feature = "std", error("Karaoke Error"))]
+pub struct KaraokeError {
+    err_type : u8,
+}
+
+impl IsFatalError for KaraokeError {
+    fn is_fatal_error(&self) -> bool {
+        return false;
+    }
+}
+
+#[cfg(feature = "std")]
+pub struct InherentDataProvider {
+    song_lines : Vec<InherentType>,
+}
+
+#[cfg(feature = "std")]
+impl InherentDataProvider {
+    pub fn new() -> Self {
+        Self { song_lines : vec!["If you like to gamble, I tell you I'm your man".as_bytes().to_vec(),
+                            "You win some, lose some, all the same to me".as_bytes().to_vec(),
+                            "The pleasure is to play, makes no difference what you say".as_bytes().to_vec(),
+                            "I don't share your greed, the only card I need is the Ace of Spades".as_bytes().to_vec()] }
+    }
+}
+
+
+#[cfg(feature = "std")]
+#[async_trait::async_trait]
+impl sp_inherents::InherentDataProvider for InherentDataProvider {
+    fn provide_inherent_data(&self, inherent_data: &mut InherentData)
+        -> Result<(), sp_inherents::Error> {
+            inherent_data.put_data(INHERENT_IDENTIFIER, &self.song_lines)
+        }
+
+    async fn try_handle_error(
+        &self,
+        _identifier : & InherentIdentifier,
+        _error : &[u8],
+        ) -> Option<Result<(), sp_inherents::Error>> {
+        None
+    }
+}
+
 
 #[frame_support::pallet]
 pub mod pallet {
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
-    use sp_karaoke::KaraokeError;
-    use sp_karaoke::INHERENT_IDENTIFIER;
-    use sp_karaoke::InherentType;
-    use sp_karaoke::SONG_LEN;
+    use super::*;
 
-
-    extern crate alloc;
-    use alloc::vec::*;
-
-    /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
@@ -28,7 +77,6 @@ pub mod pallet {
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    //#[pallet::metadata(u32 = "Metadata")]
     pub enum Event<T: Config> {
         SongLineSet([u8; 100]),
     }
