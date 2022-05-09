1a0b0f0a6e98178811a88946a756655c92740f3e
Krzysztof Ziobro <krzysztof.ziobro@cardinals.cc>
2022-05-09 11:15:10 +0200

    Karaoke: variable length extrinsic

--- pallets/karaoke/src/lib.rs
+++ pallets/karaoke/src/lib.rs
@@ -15,13 +15,34 @@ use alloc::vec::*;
 pub const INHERENT_IDENTIFIER : InherentIdentifier = *b"karaoke0";
 pub type InherentType = Vec<u8>;
 
-pub const SONG_LEN : u32 = 4;
+pub const SONG_LEN : u32 = 19;
+//const SONG : [&'static str; SONG_LEN as usize] = ["a", "b", "c", "d"];
+const SONG : [&'static str; SONG_LEN as usize] =
+["If you like to gamble, I tell you I'm your man",
+"You win some, lose some, all the same to me",
+"The pleasure is to play, makes no difference what you say",
+"I don't share your greed, the only card I need is the Ace of Spades",
+"The Ace of Spades",
+"Playing for the high one, dancing with the devil",
+"Going with the flow, it's all a game to me",
+"Seven or eleven, snake eyes watching you",
+"Double up or quit, double stake or split, the Ace of Spades",
+"The Ace of Spades",
+"You know I'm born to lose, and gambling's for fools",
+"But that's the way I like it baby",
+"I don't wanna live for ever",
+"And don't forget the joker!",
+"Pushing up the ante, I know you gotta see me",
+"Read 'em and weep, the dead man's hand again",
+"I see it in your eyes, take one look and die",
+"The only thing you see, you know it's gonna be the Ace of Spades",
+"The Ace of Spades"];
 
 #[derive(Encode, sp_runtime::RuntimeDebug)]
 #[cfg_attr(feature = "std", derive(Decode, thiserror::Error))]
 #[cfg_attr(feature = "std", error("Karaoke Error"))]
-pub struct KaraokeError {
-    err_type : u8,
+pub enum KaraokeError {
+    KaraokeError,
 }
 
 impl IsFatalError for KaraokeError {
@@ -38,10 +59,7 @@ pub struct InherentDataProvider {
 #[cfg(feature = "std")]
 impl InherentDataProvider {
     pub fn new() -> Self {
-        Self { song_lines : vec!["If you like to gamble, I tell you I'm your man".as_bytes().to_vec(),
-                            "You win some, lose some, all the same to me".as_bytes().to_vec(),
-                            "The pleasure is to play, makes no difference what you say".as_bytes().to_vec(),
-                            "I don't share your greed, the only card I need is the Ace of Spades".as_bytes().to_vec()] }
+        Self { song_lines : SONG.iter().map(|&s| s.as_bytes().to_vec()).collect() }
     }
 }
 
@@ -78,7 +96,8 @@ pub mod pallet {
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        SongLineSet([u8; 100]),
+        // Event that should be emitted when song line extrinsic is added
+        SongLineSet(InherentType),
     }
 
     #[pallet::pallet]
@@ -87,16 +106,22 @@ pub mod pallet {
 
     #[pallet::storage]
     #[pallet::getter(fn line)]
+    // Line counter that provides context for choosing a song line
     pub type Line<T> = StorageValue<_, u32, ValueQuery>;
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        #[pallet::weight((10_000, DispatchClass::Mandatory))]
-        pub fn update_karaoke_inherent(origin: OriginFor<T>, song_line : [u8; 100]) -> DispatchResult {
+        #[pallet::weight((T::DbWeight::get().reads_writes(1, 1) + 10_000, DispatchClass::Mandatory))]
+        pub fn update_karaoke_inherent(origin: OriginFor<T>, song_line : InherentType) -> DispatchResult {
+            // Check unsigned
             ensure_none(origin)?;
+
+            // update line counter
             let mut ind = Line::<T>::get();
             ind = (ind + 1) % SONG_LEN;
             Line::<T>::put(ind);
+
+            // emit an event
             Self::deposit_event(Event::SongLineSet(song_line));
             Ok(())
         }
@@ -109,18 +134,16 @@ pub mod pallet {
         const INHERENT_IDENTIFIER: InherentIdentifier = INHERENT_IDENTIFIER;
 
         fn create_inherent(data: &InherentData) -> Option<Self::Call> {
+            // extract data created by InherentDataProvider
             let data = data.get_data::<Vec<InherentType>>(&INHERENT_IDENTIFIER)
                 .expect("Song not correctly encoded")
                 .expect("Song data must be provided");
 
+            // get value of line counter from storage
             let line_num = Line::<T>::get() as usize;
 
-            let mut line_data : [u8; 100] = [0; 100];
-            for i in 0..data[line_num].len() {
-                line_data[i] = data[line_num][i];
-            }
-
-            Some(Call::update_karaoke_inherent { song_line : line_data })
+            // create inherent
+            Some(Call::update_karaoke_inherent { song_line : data[line_num].clone() })
         }
 
         fn is_inherent(call : &Self::Call) -> bool {
